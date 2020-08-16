adcebdb14056af0579f7bbc8a919ef09dded9575
Dániel Buga <bugadani@gmail.com>
2020-08-16 22:48:18 +0200

    Replace tests with examples

--- src/style/height_mode.rs
+++ src/style/height_mode.rs
@@ -18,6 +18,11 @@ use crate::{
 /// [`TextBox`]: ../../struct.TextBox.html
 pub trait HeightMode: Copy {
     /// Apply the height mode to the textbox
+    ///
+    /// *Note:* This function is used by [`TextBox::into_styled`] and normally does not need to be
+    /// called manually.
+    ///
+    /// [`TextBox::into_styled`]: ../../struct.TextBox.html#method.into_styled
     fn apply<C, F, A, V, H>(text_box: &mut StyledTextBox<'_, C, F, A, V, H>)
     where
         C: PixelColor,
@@ -28,6 +33,29 @@ pub trait HeightMode: Copy {
 }
 
 /// Keep the original height set while constructing the [`TextBox`].
+///
+/// # Example:
+///
+/// ```rust
+/// use embedded_text::prelude::*;
+/// use embedded_graphics::{fonts::Font6x8, pixelcolor::BinaryColor, prelude::*};
+///
+/// // This TextBox contains two lines of text, but is 60px high
+/// let text_box = TextBox::new(
+///     "Two lines\nof text",
+///     Rectangle::new(Point::zero(), Point::new(59, 59)),
+/// );
+///
+/// // Set style, use 6x8 font so the 2 lines are 16px high.
+/// let style = TextBoxStyleBuilder::new(Font6x8)
+///     .text_color(BinaryColor::On)
+///     .build();
+///
+/// // Exact does not change the size of the TextBox
+/// let orig_size = text_box.size();
+/// let size = text_box.into_styled(style).size();
+/// assert_eq!(size, orig_size);
+/// ```
 ///
 /// [`TextBox`]: ../../struct.TextBox.html
 #[derive(Copy, Clone, Debug)]
@@ -46,12 +74,59 @@ impl HeightMode for Exact {
     }
 }
 
-/// Change the height to exactly fit the text
+/// Sets the height of the [`StyledTextBox`] to exactly fit the text.
 ///
 /// Note: in this mode, vertical alignment is meaningless. Make sure to use [`TopAligned`] for
 /// efficiency.
 ///
+/// # Example: `FitToText` grows the [`TextBox`].
+///
+/// ```rust
+/// use embedded_text::prelude::*;
+/// use embedded_graphics::{fonts::Font6x8, pixelcolor::BinaryColor, prelude::*};
+///
+/// // This TextBox contains two lines of text, but is 1px high
+/// let text_box = TextBox::new(
+///     "Two lines\nof text",
+///     Rectangle::new(Point::zero(), Point::new(59, 0)),
+/// );
+///
+/// // Set style, use 6x8 font so the 2 lines are 16px high.
+/// let style = TextBoxStyleBuilder::new(Font6x8)
+///     .height_mode(FitToText)
+///     .text_color(BinaryColor::On)
+///     .build();
+///
+/// // FitToText grows the TextBox to the height of the text
+/// let size = text_box.into_styled(style).size();
+/// assert_eq!(size, Size::new(60, 16));
+/// ```
+///
+/// # Example: `FitToText` also shrinks the [`TextBox`].
+///
+/// ```rust
+/// use embedded_text::prelude::*;
+/// use embedded_graphics::{fonts::Font6x8, pixelcolor::BinaryColor, prelude::*};
+///
+/// // This TextBox contains two lines of text, but is 1px high
+/// let text_box = TextBox::new(
+///     "Two lines\nof text",
+///     Rectangle::new(Point::zero(), Point::new(59, 59)),
+/// );
+///
+/// // Set style, use 6x8 font so the 2 lines are 16px high.
+/// let style = TextBoxStyleBuilder::new(Font6x8)
+///     .height_mode(FitToText)
+///     .text_color(BinaryColor::On)
+///     .build();
+///
+/// // FitToText shrinks the TextBox to the height of the text
+/// let size = text_box.into_styled(style).size();
+/// assert_eq!(size, Size::new(60, 16));
+/// ```
+///
 /// [`TopAligned`]: ../../alignment/top/struct.TopAligned.html
+/// [`TextBox`]: ../../struct.TextBox.html
 #[derive(Copy, Clone, Debug)]
 pub struct FitToText;
 
@@ -71,6 +146,52 @@ impl HeightMode for FitToText {
 
 /// If the text doesn't fill the original height, shrink the [`StyledTextBox`] to be as tall as the
 /// text.
+///
+/// # Example: `ShrinkToText` does not grow the [`TextBox`].
+///
+/// ```rust
+/// use embedded_text::prelude::*;
+/// use embedded_graphics::{fonts::Font6x8, pixelcolor::BinaryColor, prelude::*};
+///
+/// // This TextBox contains two lines of text, but is 1px high
+/// let text_box = TextBox::new(
+///     "Two lines\nof text",
+///     Rectangle::new(Point::zero(), Point::new(59, 0)),
+/// );
+///
+/// // Set style, use 6x8 font so the 2 lines are 16px high.
+/// let style = TextBoxStyleBuilder::new(Font6x8)
+///     .height_mode(ShrinkToText)
+///     .text_color(BinaryColor::On)
+///     .build();
+///
+/// let size = text_box.into_styled(style).size();
+/// assert_eq!(size, Size::new(60, 1));
+/// ```
+///
+/// # Example: `ShrinkToText` shrinks the [`TextBox`].
+///
+/// ```rust
+/// use embedded_text::prelude::*;
+/// use embedded_graphics::{fonts::Font6x8, pixelcolor::BinaryColor, prelude::*};
+///
+/// // This TextBox contains two lines of text, but is 60px high
+/// let text_box = TextBox::new(
+///     "Two lines\nof text",
+///     Rectangle::new(Point::zero(), Point::new(59, 59)),
+/// );
+///
+/// // Set style, use 6x8 font so the 2 lines are 16px high.
+/// let style = TextBoxStyleBuilder::new(Font6x8)
+///     .height_mode(ShrinkToText)
+///     .text_color(BinaryColor::On)
+///     .build();
+///
+/// let size = text_box.into_styled(style).size();
+/// assert_eq!(size, Size::new(60, 16));
+/// ```
+///
+/// [`TextBox`]: ../../struct.TextBox.html
 #[derive(Copy, Clone, Debug)]
 pub struct ShrinkToText;
 
@@ -85,105 +206,5 @@ impl HeightMode for ShrinkToText {
         H: HeightMode,
     {
         text_box.fit_height_limited(text_box.size().height);
-    }
-}
-
-#[cfg(test)]
-mod test_styled {
-    use super::*;
-    use crate::{
-        prelude::*,
-        style::height_mode::{FitToText, ShrinkToText},
-    };
-    use embedded_graphics::{fonts::Font6x8, pixelcolor::BinaryColor};
-
-    #[test]
-    fn test_exact() {
-        // way too high text box
-        let text_box = TextBox::new(
-            "Two lines\nof text",
-            Rectangle::new(Point::zero(), Point::new(59, 60)),
-        );
-
-        let style = TextBoxStyleBuilder::new(Font6x8)
-            .text_color(BinaryColor::On)
-            .build();
-
-        let orig_size = text_box.size();
-        let size = text_box.into_styled(style).size();
-
-        assert_eq!(size, orig_size);
-    }
-
-    #[test]
-    fn test_shrink_bigger() {
-        // way too high text box
-        let text_box = TextBox::new(
-            "Two lines\nof text",
-            Rectangle::new(Point::zero(), Point::new(59, 60)),
-        );
-
-        let style = TextBoxStyleBuilder::new(Font6x8)
-            .text_color(BinaryColor::On)
-            .height_mode(ShrinkToText)
-            .build();
-
-        let size = text_box.into_styled(style).size();
-
-        assert_eq!(size, Size::new(60, 16));
-    }
-
-    #[test]
-    fn test_shrink_smaller() {
-        // way too high text box
-        let text_box = TextBox::new(
-            "Two lines\nof text",
-            Rectangle::new(Point::zero(), Point::new(59, 7)),
-        );
-
-        let style = TextBoxStyleBuilder::new(Font6x8)
-            .text_color(BinaryColor::On)
-            .height_mode(ShrinkToText)
-            .build();
-
-        let size = text_box.into_styled(style).size();
-
-        assert_eq!(size, Size::new(60, 8));
-    }
-
-    #[test]
-    fn test_fit_bigger() {
-        // way too high text box
-        let text_box = TextBox::new(
-            "Two lines\nof text",
-            Rectangle::new(Point::zero(), Point::new(59, 60)),
-        );
-
-        let style = TextBoxStyleBuilder::new(Font6x8)
-            .text_color(BinaryColor::On)
-            .height_mode(FitToText)
-            .build();
-
-        let size = text_box.into_styled(style).size();
-
-        assert_eq!(size, Size::new(60, 16));
-    }
-
-    #[test]
-    fn test_fit_smaller() {
-        // way too high text box
-        let text_box = TextBox::new(
-            "Two lines\nof text",
-            Rectangle::new(Point::zero(), Point::new(59, 0)),
-        );
-
-        let style = TextBoxStyleBuilder::new(Font6x8)
-            .text_color(BinaryColor::On)
-            .height_mode(FitToText)
-            .build();
-
-        let size = text_box.into_styled(style).size();
-
-        assert_eq!(size, Size::new(60, 16));
     }
 }
