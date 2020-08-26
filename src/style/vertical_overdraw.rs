042a20baff7bb1a94b1e9739f4f8c3de970ece39
Dániel Buga <bugadani@gmail.com>
2020-08-26 20:14:19 +0200

    Add test for default behaviour

--- src/style/vertical_overdraw.rs
+++ src/style/vertical_overdraw.rs
@@ -1,4 +1,4 @@
-//!
+//! Vertical overdraw options.
 
 /// Implementors of this trait specify how drawing vertically outside the bounding box is handled.
 pub trait VerticalOverdraw {}
@@ -14,3 +14,50 @@ impl VerticalOverdraw for Hidden {}
 /// Display text even if it's outside the bounding box.
 pub struct Visible;
 impl VerticalOverdraw for Visible {}
+
+#[cfg(test)]
+mod test {
+    use embedded_graphics::{
+        fonts::Font6x8, mock_display::MockDisplay, pixelcolor::BinaryColor, prelude::*,
+        primitives::Rectangle,
+    };
+
+    use crate::{
+        alignment::{BottomAligned, LeftAligned},
+        style::TextBoxStyleBuilder,
+        TextBox,
+    };
+
+    #[test]
+    fn default_is_full_rows_only() {
+        // This test verifies that FullRowsOnly does not draw partial rows
+
+        let mut display = MockDisplay::new();
+        let style = TextBoxStyleBuilder::new(Font6x8)
+            .alignment(LeftAligned)
+            .text_color(BinaryColor::On)
+            .build();
+
+        TextBox::new(
+            "word and other words",
+            Rectangle::new(Point::new(0, 0), Point::new(54, 14)),
+        )
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+        assert_eq!(
+            display,
+            MockDisplay::from_pattern(&[
+                "                      #                       #",
+                "                      #                       #",
+                "#   #  ###  # ##   ## #        ###  # ##   ## #",
+                "#   # #   # ##  # #  ##           # ##  # #  ##",
+                "# # # #   # #     #   #        #### #   # #   #",
+                "# # # #   # #     #   #       #   # #   # #   #",
+                " # #   ###  #      ####        #### #   #  ####",
+                "                                               ",
+            ])
+        );
+    }
+}
