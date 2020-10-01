a32ec0aae9149587199eb7a66645f646e97141c2
Dániel Buga <bugadani@gmail.com>
2020-10-01 19:31:55 +0200

    Work around for rustc issue

--- src/parser.rs
+++ src/parser.rs
@@ -112,7 +112,13 @@ impl<'a> Iterator for Parser<'a> {
                     '\n' => Some(Token::NewLine),
                     '\r' => Some(Token::CarriageReturn),
                     '\u{200B}' => Some(Token::Break(None)),
-                    '\u{00AD}' => Some(Token::Break(Some('-'))),
+                    '\u{00AD}' => {
+                        // work around rustc issue #77417
+                        if iter.next() == Some('\u{00AD}') {
+                            self.inner = iter.clone();
+                        }
+                        Some(Token::Break(Some('-')))
+                    }
 
                     _ => {
                         let mut len = 1;
@@ -213,6 +219,21 @@ mod test {
         assert_eq!(
             Parser::parse(" \u{A0}word").collect::<Vec<Token>>(),
             vec![Token::Whitespace(1), Token::Word("\u{A0}word"),]
+        );
+    }
+
+    #[test]
+    fn parse_shy_issue_42() {
+        let text = "f\u{AD}­cali";
+        println!("{:?}", "f\u{AD}­cali");
+
+        assert_eq!(
+            Parser::parse(text).collect::<Vec<Token>>(),
+            vec![
+                Token::Word("f"),
+                Token::Break(Some('-')),
+                Token::Word("cali"),
+            ]
         );
     }
 }
