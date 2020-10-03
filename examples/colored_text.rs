b60843359aef49180002348c0ef4862b5ccd3f08
Dániel Buga <bugadani@gmail.com>
2020-10-03 23:14:34 +0200

    Small styling update

--- examples/colored_text.rs
+++ examples/colored_text.rs
@@ -3,21 +3,24 @@ use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Windo
 use embedded_text::prelude::*;
 
 fn main() -> Result<(), core::convert::Infallible> {
-    let text = format!("{comment}/// Comment\n\
+    let text = format!(
+        "{comment}/// Comment\n\
         {base_text}#[{attribute}derive{base_text}(Debug)]\n\
         {keyword}enum {type_name}Foo{base_text}<{lifetime}'a{base_text}> {{\n\
         {comment}\t/// Decide what to do next.\n\
         {highlighted_background}\t{enum_variant}Bar{base_text}({type_name}Token{base_text}<{lifetime}'a{base_text}>),                      \n\
         {line_background}{base_text}}}",
+        // colors
         line_background = "\x1b[48;5;16m",
         highlighted_background = "\x1b[48;5;235m",
         enum_variant = "\x1b[38;2;36;144;241m",
         keyword = "\x1b[38;2;84;128;166m",
         comment = "\x1b[38;2;94;153;73m",
         base_text = "\x1b[97m",
-        attribute="\x1b[38;2;220;220;157m",
+        attribute ="\x1b[38;2;220;220;157m",
         type_name = "\x1b[38;2;78;201;176m",
-        lifetime = "\x1b[38;2;84;128;166m");
+        lifetime = "\x1b[38;2;84;128;166m"
+    );
 
     let textbox_style = TextBoxStyleBuilder::new(Font6x8)
         .text_color(Rgb888::BLACK)
