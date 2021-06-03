d129e844b5941e4979bf16fff83692e361853892
Dániel Buga <bugadani@gmail.com>
2021-06-03 20:45:17 +0200

    Merge color example into styles-static

--- examples/styles-static.rs
+++ examples/styles-static.rs
@@ -1,17 +1,18 @@
-//! This example demonstrates additional text decoration options (underlined and strike-through text).
+//! #Example: static styles
+//!
+//! This example demonstrates additional text decoration options (underlined and strike-through
+//! text, text background).
 
 use std::convert::Infallible;
 
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
-    pixelcolor::BinaryColor,
+    pixelcolor::Rgb888,
     prelude::*,
     primitives::Rectangle,
     text::LineHeight,
 };
-use embedded_graphics_simulator::{
-    BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
-};
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
 use embedded_text::{
     alignment::VerticalAlignment,
     style::{HeightMode, TextBoxStyleBuilder},
@@ -21,24 +22,29 @@ use embedded_text::{
 fn main() -> Result<(), Infallible> {
     let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
 
-    // Specify the common styling options:
-    // * Use the 6x8 MonoFont from embedded-graphics.
-    // * Draw the text horizontally left aligned (default option, not specified here).
-    // * Use `FitToText` height mode to stretch the text box to the exact height of the text.
-    // * Draw the text with `BinaryColor::On`, which will be displayed as light blue.
-    let character_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On);
-
     let text_box_style = TextBoxStyleBuilder::new()
         .vertical_alignment(VerticalAlignment::Scrolling)
         .height_mode(HeightMode::FitToText)
         .line_height(LineHeight::Pixels(12))
         .build();
 
-    // Specify underlined and strike-through decorations, one for each text box.
-    let underlined_style = character_style.underline().build();
-    let strikethrough_style = character_style.strikethrough().build();
+    let underlined_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(Rgb888::WHITE)
+        .underline_with_color(Rgb888::GREEN)
+        .build();
+
+    let strikethrough_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(Rgb888::WHITE)
+        .strikethrough_with_color(Rgb888::RED)
+        .build();
+
+    let background_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(Rgb888::WHITE)
+        .background_color(Rgb888::CSS_STEEL_BLUE)
+        .build();
 
     let text_box = TextBox::with_textbox_style(
         text,
@@ -54,24 +60,33 @@ fn main() -> Result<(), Infallible> {
         text_box_style,
     );
 
+    let text_box3 = TextBox::with_textbox_style(
+        text,
+        Rectangle::new(Point::new(192, 0), Size::new(97, 0)),
+        background_style,
+        text_box_style,
+    );
+
     // Create a window for both text boxes.
     let mut display = SimulatorDisplay::new(Size::new(
-        text_box.bounding_box().size.width + text_box2.bounding_box().size.width,
+        text_box.bounding_box().size.width
+            + text_box2.bounding_box().size.width
+            + text_box3.bounding_box().size.width,
         text_box
             .bounding_box()
             .size
             .height
-            .max(text_box2.bounding_box().size.height),
+            .max(text_box2.bounding_box().size.height)
+            .max(text_box3.bounding_box().size.height),
     ));
 
     // Draw the text boxes.
     text_box.draw(&mut display)?;
     text_box2.draw(&mut display)?;
+    text_box3.draw(&mut display)?;
 
     // Set up the window and show the display's contents.
-    let output_settings = OutputSettingsBuilder::new()
-        .theme(BinaryColorTheme::OledBlue)
-        .build();
+    let output_settings = OutputSettingsBuilder::new().scale(2).build();
     Window::new("Hello TextBox", &output_settings).show_static(&display);
 
     Ok(())
